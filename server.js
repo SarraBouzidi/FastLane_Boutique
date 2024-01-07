5973f41ec51f0624098c5b9020cbd0139849b4cd
SarraBouzidi <sarra.bouzidi@isamm.u-manouba.tn>
2024-01-07 15:03:59 +0100

    correction server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-import express from "express";
+import express from 'express';
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+//rest object
+const app = express();
 
 //configure env
 dotenv.config();
@@ -13,8 +15,6 @@ connectDB();
 //middelwares
 app.use(express.json());
 app.use(morgan("dev"));
-//rest object
-const app = express();
 
 //rest api
 app.get("/", (req, res) => {
