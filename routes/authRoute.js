8d5be3bbd4822ee25b0e838a7d4a83dd01611bbe
SarraBouzidi <sarra.bouzidi@isamm.u-manouba.tn>
2024-01-07 15:38:14 +0100

    ajoute routers

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,8 @@
 import express from "express";
-
+import {registerController} from "../controllers/authController.js"
 //router object
 const router = express.Router();
-
+// routing
+//REGISTER || METHOD POST
+router.post('/register',registerController)
 export default router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js"
 //rest object
 const app = express();
 
@@ -15,7 +16,8 @@ connectDB();
 //middelwares
 app.use(express.json());
 app.use(morgan("dev"));
-
+// routes
+app.use('/api/v1/auth',authRoutes);
 //rest api
 app.get("/", (req, res) => {
     res.send("<h1>Welcome to ecommerce app</h1>");
