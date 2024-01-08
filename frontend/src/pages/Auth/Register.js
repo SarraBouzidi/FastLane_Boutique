4e2a19b0effc22153dfdc2e2605a88827b71a0ae
SarraBouzidi <sarra.bouzidi@isamm.u-manouba.tn>
2024-01-08 19:07:18 +0100

    up

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import Layout from '../../components/Layout/Layout'
+import Layout from '../../components/Layout/Layout';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 const Register = () => {
     const [name, setName] = useState("");
@@ -7,13 +9,32 @@ const Register = () => {
     const [password, setPassword] = useState("");
     const [phone, setPhone] = useState("");
     const [address, setAddress] = useState("");
-    const [answer, setAnswer] = useState("");
+
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(name, email, password, address);
-        toast.success("Register successfully");
+        try {
+            const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/register`, {
+                name,
+                email,
+                password,
+                phone,
+                address,
+            });
+            if (res.data.success) {
+                toast.success(res.data.message);
+                navigate("/login");
+            } else {
+                toast.error(res.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("Something went wrong");
+
+        }
     };
+    console.log(process.env.REACT_APP_API);
     return (
         <Layout title="Register Page">
             <div className='register'>
@@ -72,6 +93,7 @@ const Register = () => {
                             placeholder="Enter Your Address"
                             required />
                     </div>
+
                     <button type="submit" className="btn btn-primary">
                         Submit
                     </button>
