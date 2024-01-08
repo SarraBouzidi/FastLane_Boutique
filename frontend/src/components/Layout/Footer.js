31cb787de9745fe79160c7d8d9494f21e812b923
SarraBouzidi <sarra.bouzidi@isamm.u-manouba.tn>
2024-01-08 03:40:12 +0100

    update front

--- frontend/src/components/Layout/Footer.js
+++ frontend/src/components/Layout/Footer.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const Footer = () => {
-    const styles = {
-        backgroundColor: '#414143',
-        color: '#ffffff',
-        padding: '1rem', // Adjust padding as needed
-        textAlign: 'center', // Center the text
-      };
+  const styles = {
+    backgroundColor: '#414143',
+    color: '#ffffff',
+    padding: '1rem', // Adjust padding as needed
+    textAlign: 'center', // Center the text
+  };
   return (
     <div style={styles}>
-        <h1 className='text-center'>All Right Reserved &copy; FastLane Boutique 2024  </h1>
+      <h1 className='text-center'>All Right Reserved &copy; FastLane Boutique 2024  </h1>
     </div>
   )
 }
--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,34 +1,51 @@
 import React from 'react';
-import{NavLink}from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
+import logo from './logo_shop.png';
+
 const Header = () => {
   return (
     <>
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <NavLink to='/' className="navbar-brand" href="#">Navbar</NavLink>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink to='/' className="nav-link active" aria-current="page" href="#">Home</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to='/'className="nav-link" href="#">Link</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to='/' className="nav-link disabled" href="#" tabIndex={-1} aria-disabled="true">Disabled</NavLink>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
- </>
+      <nav className="navbar navbar-expand-lg navbar-light ">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <Link to='/' className="navbar-brand" >
+            <img src={logo} alt="Logo" className="logoStyle img-fluid" />
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink to='/' className="nav-link "  >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to='/category' className="nav-link " >
+                  Category
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to='/register' className="nav-link">
+                  SignUp
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to='/login' className="nav-link" >
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to='/carte' className="nav-link" >
+                  shoie {0}
+                </NavLink>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+      </nav>
+    </>
   )
 }
 
