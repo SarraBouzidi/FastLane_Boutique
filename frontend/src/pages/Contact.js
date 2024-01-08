03b9b8f63d5658955542f58a44916f1107b604c4
SarraBouzidi <sarra.bouzidi@isamm.u-manouba.tn>
2024-01-08 05:36:57 +0100

    update

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,22 +1,20 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
-import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+import { BiMailSend, BiPhoneCall, BiSupport } from 'react-icons/bi';
+
 const Contact = () => {
   return (
-    <Layout title={"Contact us"}>
-      <div className="row contactus ">
-        <div className="col-md-6 ">
-          <img
-            src="/images/contactus.jpeg"
-            alt="contactus"
-            style={{ width: "100%" }}
-          />
+    <Layout>
+      <div className="row contactus">
+        <div className="col-md-6">
+          <img src="/images/contactus.jpeg" alt="contactus" className="img-fluid" style={{ width: "92%" }} />
         </div>
-        <div className="col-md-4">
-          <h1 className=" p-2 text-white text-center" style={{ backgroundColor: '#FCB041' }}>CONTACT US</h1>
+        <div className="col-md-4 ">
+          <h1 className="p-2 text-white text-center" style={{ backgroundColor: '#FCB041' }}>
+            CONTACT US
+          </h1>
           <p className="text-justify mt-2">
-            any query and info about prodduct feel free to call anytime we 24X7
-            vaialible
+            Any query and information about products, feel free to call anytime; we're available 24/7.
           </p>
           <p className="mt-3">
             <BiMailSend /> : www.help@ecommerceapp.com
@@ -30,7 +28,7 @@ const Contact = () => {
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- frontend/src/pages/Policy.js
+++ frontend/src/pages/Policy.js
@@ -3,10 +3,26 @@ import Layout from '../components/Layout/Layout'
 
 const Policy = () => {
   return (
-    <Layout>
-        <h1>Policy</h1>
+    <Layout title={"Privacy Policy"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/policy.svg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <h1 className="p-2 text-white text-center" style={{ backgroundColor: '#FCB041' }}>
+            Privacy Policy          </h1>
+
+          <p>FastLane ("us", "we", or "our") operates FastLane Boutique (the "Service"). This page informs you of our policies regarding the collection, use, and disclosure of Personal Information when you use our Service.
+            We will not use or share your information with anyone except as described in this Privacy Policy.
+            We use your Personal Information for providing and improving the Service. By using the Service, you agree to the collection and use of information in accordance with this policy.</p>
+        </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
 export default Policy
