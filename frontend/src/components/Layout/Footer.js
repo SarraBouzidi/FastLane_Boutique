71a6fa02e12fd78ded6fe866c88b4a33cc48273d
SarraBouzidi <sarra.bouzidi@isamm.u-manouba.tn>
2024-01-08 04:14:39 +0100

    update

--- frontend/src/components/Layout/Footer.js
+++ frontend/src/components/Layout/Footer.js
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 const Footer = () => {
-  const styles = {
-    backgroundColor: '#414143',
-    color: '#ffffff',
-    padding: '1rem', // Adjust padding as needed
-    textAlign: 'center', // Center the text
-  };
+
   return (
-    <div style={styles}>
+    <div className="footer" >
       <h1 className='text-center'>All Right Reserved &copy; FastLane Boutique 2024  </h1>
+      <p className='text-center mt-3'>
+        <Link to="/about">About</Link>|
+        <Link to="/contact">Contact</Link>|
+        <Link to="/policy">Privacy Policy</Link>
+      </p>
     </div>
   )
 }
--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <div>
-       <Header/>
-        <main style={{ minHeight: "80vh" }}>
-            {children}
-        </main>
-        <Footer/>
+      <Header />
+      <main style={{ minHeight: "70vh" }}>
+        {children}
+      </main>
+      <Footer />
     </div>
   );
 }
